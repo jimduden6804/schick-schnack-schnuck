3642c1e33c87732bb91bb183a5d906f0c85e9955
Jim Duden <jim.duden@otto.de>
2021-06-01 17:26:53 +0200

    JD | initial commit is initial

---
+++ test/schnick_schnack_schnuck/core_test.clj
@@ -0,0 +1,7 @@
+(ns schnick-schnack-schnuck.core-test
+  (:require [clojure.test :refer :all]
+            [schnick-schnack-schnuck.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
